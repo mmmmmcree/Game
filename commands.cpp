c9c0be8e47351e2647889ec09a3870c9f74060c4
MMMMMCREE <1349751341@qq.com>
2023-05-15 17:00:21 +0800

    23.5.15

--- commands.cpp
+++ commands.cpp
@@ -4,20 +4,36 @@
 #include<iostream>
 #include<conio.h>
 
+void loop_control(std::vector<Command*> &slots, int (*trans)(char)){
+    int i;
+    for(char c; std::cout << "command: ", c = _getche(), std::cout << std::endl, c != '~';){
+        i = trans(c);
+        if(i < slots.size() && i >= 0){slots[trans(c)]->execute();}
+        else{std::cout << "无效命令" << std::endl;}
+    }
+}
+
+void loop_control(std::vector<Command*> &slots, int (*trans)(char), int n){
+    int i;
+    for(char c; std::cout << "command: ", c = _getche(), std::cout << std::endl, c != '~';){
+        i = trans(c);
+        if(i < slots.size() && i >= 0){slots[trans(c)]->execute(n);}
+        else{std::cout << "无效命令" << std::endl;}
+    }
+} 
+
 Main_Command::Main_Command():
     slots{new Character_Command}{}
 
 void Main_Command::execute(){
     std::cout << "MODE: Main_Command" << std::endl;
-    for(char c;std::cout << "command: ", c = _getche(), std::cout << std::endl, c != '~';){
-        slots[0]->execute();
-    }
+    loop_control(slots, trans_main);
 }
 
 Character_Command::Character_Command():
     slots{new Create_Command, new Attack_Command, }{}
 
-int Character_Command::choose_position(){
+int Character_Command::choose_position(){ //todo 妈的，这里必须改
     ENTER: std::cout << "选择操纵位置: ";
     std::vector<Character*>::size_type n = _getche() - '0' - 1;
     for(n; n < 0 || n > 2; n = _getche() - '0' - 1){
@@ -42,13 +58,15 @@ int Character_Command::choose_position(){
 void Character_Command::execute(){
     std::cout << "MODE: Character_Command" << std::endl;
     int n = choose_position();
-    for(char c; std::cout << "command: ", c = _getche(), std::cout << std::endl, c != '~';){
-        slots[1]->execute(n);
-    }
+    loop_control(slots, trans_character, n);
     std::cout << "Mode: Main_Mode" << std::endl;
 }
 
 void Create_Command::execute(int n){
+    if(Controller::troop[n]){
+        std::cout << "当前位置已有士兵，无法创建新的士兵" << std::endl;
+        return;
+    }
     std::string name;
     std::cout << "输入士兵[" << n << "]的名字: ";
     std::getline(std::cin, name);
@@ -57,4 +75,25 @@ void Create_Command::execute(int n){
 
 void Attack_Command::execute(int n){
     Controller::troop[n]->attack();
+}
+
+
+int trans_main(char c){
+    switch (c){
+    case 'p':
+        return 0;
+    default:
+        return -1;
+    }    
+}
+
+int trans_character(char c){
+    switch (c){
+    case 'c':
+        return 0;
+    case 'j':
+        return 1;
+    default:
+        return -1;
+    }
 }
--- commands.h
+++ commands.h
@@ -2,12 +2,15 @@
 *   一共有两种Command类: 具槽类和不具槽类
 *   具槽类一般是选择命令的状态，如Main_Command模式，在该模式下可以选择其他命令
 *   不具槽类一般是基本命令，如Attack_Command
-*   具槽类需要具有std::vector<Command*> slots 以及对应字母到数字的转换函数
+*   具槽类需要具有std::vector<Command*> slots 以及对应字母到数字的转换函数(转换函数不做为成员，而是单独定义)
 */
 #ifndef COMMANDS_H
 #define COMMANDS_H
 #include<vector>
 #include"command.h"
+
+void loop_control(std::vector<Command*> &, int (*)(char)); //* 用于具槽命令类的循环控制
+void loop_control(std::vector<Command*> &, int (*)(char), int);
 
 class Main_Command : public Command{
     std::vector<Command*> slots;
@@ -16,6 +19,8 @@ public:
     void execute();
 };
 
+int trans_main(char);
+
 class Character_Command : public Command{
     std::vector<Command*> slots;
 public:
@@ -23,6 +28,8 @@ public:
     void execute();
     int choose_position();
 };
+
+int trans_character(char); //* Character_Command的转换函数
 
 class Create_Command : public Command{
 public:
